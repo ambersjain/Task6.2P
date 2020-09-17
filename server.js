6ed67eb6b9f624f77e6d968c262a29b78899cfa7
Amber Jain <jainamb@deakin.edu.au>
2020-09-17 21:38:52 +1000

    Added the required routes

--- server.js
+++ server.js
@@ -28,9 +28,12 @@ db.once('open', function () {
   console.log('Connected to MongoDB');
 })
 
-// Model
+// User Model
 const User = require("./models/user");
 const { EEXIST } = require('constants');
+
+// Worker Model
+const Worker = require("./models/worker");
 
 // Need to use this to be able to use views folder
 app.use(express.static('views'));
@@ -62,9 +65,9 @@ app.get('/welcome', (req, res) => {
 
 
 // 404 route
-app.get('/*', (req, res) => {
-  res.sendFile(`${base}/404.html`);
-});
+// app.get('/*', (req, res) => {
+//   res.sendFile(`${base}/404.html`);
+// });
 
 // Signup
 app.post('/', (req, res) => {
@@ -139,8 +142,8 @@ app.post('/', (req, res) => {
 
 //Login
 app.post('/signin', (req, res) => {
-  User.User.findOne({email: req.body.email}, (err, user)=> {
-    if(err) {
+  User.User.findOne({ email: req.body.email }, (err, user) => {
+    if (err) {
       console.log(err);
       return res.status(500).send();
     }
@@ -164,6 +167,98 @@ app.post('/signin', (req, res) => {
     return res.status(200).send();
   })
 })
+
+
+/*
+    WORKER ROUTES
+*/
+
+
+/* USE GET, POST and DELETE TO
+  - Retrieve, add and remove workers respectively[URI: /workers]
+*/
+
+
+app.route('/workers')
+  .get((req, res) => {
+    Worker.Worker.find((err, workerList) => {
+      if (err) { res.send(err) }
+      else { res.send(workerList) }
+    })
+
+  })
+  .post((req, res) => {
+    const worker = new Worker.Worker({
+      id: req.body.id,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      address: req.body.address,
+      mobile_number: req.body.mobile_number,
+    });
+
+    worker.save((err) => {
+      if (err) { res.send(err); console.log("Unable to add the worker"); }
+      else { res.send("Worker Added Succesfully") }
+    })
+  })
+  .delete((req, res) => {
+    Worker.Worker.deleteMany((err) => {
+      if (err) { res.send(err) }
+      else { res.send("Successfully deleted all worker records!") }
+    })
+  });
+
+
+/* USE GET, PUT and DELETE TO
+  - Retrieve, update and remove a specific worker [URI: /workers/:id]
+*/
+
+/* USE PATCH TO
+  - Update specific worker’s address and mobile phone [URI: /workers/:id]
+  - Update a specific worker’s password [URI: /workers/:id]
+*/
+app.route('/workers/:id')
+  .get((req, res) => {
+    Worker.Worker.findOne({ id: req.params.id }, (err, foundWorker) => {
+      if (foundWorker) { res.send(foundWorker) }
+      else { res.send("Worker not found.") }
+    })
+  })
+  .put((req, res) => {
+    Worker.Worker.update(
+      { id: req.params.id },
+      {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        address: req.body.address,
+        mobile_number: req.body.mobile_number
+      },
+      { overwrite: true },
+      (err) => {
+        if (err) { res.send(err) }
+        else { res.send("Successfully Updated!") }
+      }
+    )
+  })
+  .delete((req, res) => {
+    Worker.Worker.deleteOne({ id: req.params.id }, (err, foundWorker) => {
+      if (foundWorker) { res.send(foundWorker) }
+      else { res.send("Worker account not deleted") }
+    })
+  })
+  .patch((req, res) => {
+    Worker.Worker.update(
+      { id: req.params.id },
+      { $set: req.body },
+      { overwrite: true },
+      (err) => {
+        if (err) { res.send(err) }
+        else { res.send("Successfully Updated!") }
+      }
+    )
+  })
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
